5f8c0068edf19905597a80d19fe86298d47b7c27
Ethan Chapman <ethan@we3chapmans.com>
2022-07-29 22:15:31 -0400

    First JavaFX working

--- app/src/main/java/chokistream/App.java
+++ app/src/main/java/chokistream/App.java
@@ -1,7 +1,26 @@
 package chokistream;
 
-public class App {
-    public static void main(String[] args) {
-    	
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
+    
+    public static void main(String[] args) {
+    	launch();
+    }
+
 }
