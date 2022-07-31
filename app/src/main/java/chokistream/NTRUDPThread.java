ad10c77b3495d1c58f15aad29c191afca97a0b67
ChainSwordCS <chainswordcs@gmail.com>
2022-07-31 02:41:12 -0400

    First draft of hotfixColors stuff
    Not tested. There WILL be bugs and unintended side effects and it's not fully optimized yet. I didn't even attempt to compile yet. I'm going to bed and would prefer not to lose my work.

--- app/src/main/java/chokistream/NTRUDPThread.java
+++ app/src/main/java/chokistream/NTRUDPThread.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage; // For hotfixColors method
 
 /**
  * 
@@ -68,23 +69,62 @@ public class NTRUDPThread extends Thread {
 	 * Attempt to hot-fix the colors if the option is enabled(?)
 	 * This is specifically aimed at Virtual Console games:
 	 * Most notably Pokemon Yellow, but also Super Mario Bros, etc.
+	 * 
+	 * Theoretically can handle images with arbitrary dimensions and resolutions.
+	 * If I implement it right, the Pixel Format of
+	 * the hotfixImageInput shouldn't matter.
+	 * 
+	 * Assumes the input image's dimensions are each not 0 (or negative or null).
 	 */
 	private Image hotfixColors(Image hotfixImageInput) {
+		WritableImage hotfixImageWritable = new WritableImage( (int)hotfixImageInput.getWidth() , (int)hotfixImageInput.getHeight() );
 		Image hotfixImageOutput = null;
+		int currentPixelArgbBefore = 0;
+		int currentPixelArgbAfter = 0;
 		int currentW = 0; // Current Width (position of pixel being processed)
 		int currentH = 0; // Current Height
-		int pixelsConverted = 0;
-		int numPixelsMax = (int)( (hotfixImageInput.getHeight() * (int)(hotfixImageInput.getWidth()) ) );
-		
-		while (pixelsConverted < numPixelsMax) {
-			while (currentH < 0) {
-				pixelsConverted++;
+		/** 
+		 * Pixel processing and transferring loops. Note they are zero-indexed.
+		 * Also note. JFX has functions to automatically transfer pieces of the image,
+		 * But we can't use that, because we need to play with the bit order ),:
+		 */
+		while (currentH < hotfixImageInput.getHeight()) {
+			while (currentW < hotfixImageInput.getWidth()) {
+/**
+ * Logic for hot-fixing image colors! I am the king of hexadecimal. -C
+ * 
+ * Guide to what's what... (What this code intends to do.)
+ * Letters ARGB stand for Alpha,Red,Green,Blue respectively.
+ * Each group of eight letters is a byte. Each letter is a Bit.
+ * The letter indicates which of the four the Bit contributes value to.
+ * 
+ * ...Did I explain that badly? Was that overly confusing? Oh well... -C
+ *
+ * What we receive (ARGB):AAAAAAAA RRRRRRRR GGGGGGGG  B  B  B  B B  BBB
+ *                                 |||||||| ||||||||  |  |  |  | ?
+ * How we (INTEND TO)              |||||||| ||||||||  |_ |_ |_ |_
+ * shift bits:                     |||||||| ||||||||    |  |  |  |
+ *                                 VVVVVVVV VVVVVVVV  ? V  V  V  V
+ * What we draw (ARGB):   11111111 RRRRRRRR GGGGGGGG  B B  B  B  B  BBB
+ */
+				currentPixelArgbBefore = hotfixImageInput.getPixelReader().getArgb(currentW, currentH);
+				
+				// Here's the magic. On second thought, this should definitely be optimized with bitwise-AND,OR,NOT,XOR,etc.
+				int newBluPixel = ((int)((currentPixelArgbBefore%256)/16))*8 + (int)((currentPixelArgbBefore%256)/32);
+				newBluPixel = newBluPixel + ((int)(currentPixelArgbBefore/8)%2)*128;
+				currentPixelArgbAfter = ((int)(currentPixelArgbBefore/256))*256 + newBluPixel;
+				
+				hotfixImageWritable.getPixelWriter().setArgb(currentW,currentH,currentPixelArgbAfter);
+				currentW++;
 			}
+			currentW = 0;
+			currentH++;
 		}
-		
 		return hotfixImageOutput;
 	}
-	
+	/**
+	 * Main code block ^.^
+	 */
 	public void run() {
 		while (!shouldDie.get()) {
 			DatagramPacket packet = new DatagramPacket(packetBuffer, packetBuffer.length);
