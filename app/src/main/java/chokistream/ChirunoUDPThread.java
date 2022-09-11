3a312bc455bb2041501f5ac6ed6008b82a33ffbc
ChainSwordCS <chainswordcs@gmail.com>
2022-09-10 22:36:23 -0400

    ChirunoMod UDP - Step 1 of 99

---
+++ app/src/main/java/chokistream/ChirunoUDPThread.java
@@ -0,0 +1,179 @@
+package chokistream;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Arrays;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.imageio.ImageIO;
+
+import chokistream.props.ColorMode;
+import chokistream.props.DSScreen;
+import chokistream.props.InterpolationMode;
+import chokistream.props.LogLevel;
+
+/**
+ * 
+ */
+public class ChirunoUDPThread extends Thread {
+	
+	/**
+	 * Socket used to receive UDP packets from the 3DS on.
+	 */
+	private DatagramSocket socket;
+	
+	/**
+	 * Buffer used to store UDP data in.
+	 */
+	private byte[] packetBuffer = new byte[1448]; // (MTU)
+	
+	/**
+	 * A BlockingQueue to buffer received Frames in.
+	 */
+	private BlockingQueue<Frame> frameBuffer = new LinkedBlockingQueue<Frame>();
+	
+	/**
+	 * Should the thread die?
+	 */
+	private AtomicBoolean shouldDie = new AtomicBoolean(false);
+	
+	WritableInputStream priorityInputStream = new WritableInputStream();
+	WritableInputStream secondaryInputStream = new WritableInputStream();
+	private BufferedImage priorityImage;
+	private BufferedImage secondaryImage;
+	private byte priorityExpectedFrame = 0;
+	private byte secondaryExpectedFrame = 0;
+	private byte priorityExpectedPacket = 0;
+	private byte secondaryExpectedPacket = 0;
+	private DSScreen activeScreen = DSScreen.TOP;
+	private ColorMode colorMode;
+	private double topScale;
+	private double bottomScale;
+	private InterpolationMode intrp;
+	
+	private static final Logger logger = Logger.INSTANCE;
+	
+	/**
+	 * Create an NTRUDPThread.
+	 * @throws SocketException
+	 */
+	ChirunoUDPThread(DSScreen screen, ColorMode colorMode, double topScale, double bottomScale, InterpolationMode intrp) throws SocketException {
+		activeScreen = screen;
+		socket = new DatagramSocket(6464);
+		this.colorMode = colorMode;
+		this.topScale = topScale;
+		this.bottomScale = bottomScale;
+		this.intrp = intrp;
+	}
+	
+	public Frame getFrame() throws InterruptedException {
+		return frameBuffer.take();
+	}
+	
+	public void close() {
+		shouldDie.set(true);
+		socket.close();
+	}
+
+	@Override
+	public void run() {
+		while (!shouldDie.get()) {
+			DatagramPacket packet = new DatagramPacket(packetBuffer, packetBuffer.length);
+			try {
+				socket.receive(packet);
+				byte[] data = packet.getData();
+				
+				logger.log("Attempting to receive packet", LogLevel.VERBOSE);
+				
+				if (data[0] != 0)
+				{
+					logger.log("data[0] != 0 (:", LogLevel.VERBOSE);
+				}
+				
+				byte currentFrame = data[0];
+				DSScreen currentScreen = ((data[1] & 0x0F) == 1) ? DSScreen.TOP : DSScreen.BOTTOM;
+				boolean isLastPacket = (((data[1] & 0xF0) >> 4) == 1);
+				int currentPacket = data[3];
+				
+				logger.log("Recieved packet for screen "+currentScreen.getLongName()+
+						", isLast="+isLastPacket+", curF="+currentFrame+", curP="+currentPacket, LogLevel.VERBOSE);
+				logger.log(data, LogLevel.EXTREME);
+				
+				if (priorityExpectedFrame == 0 && currentScreen == activeScreen) {
+					priorityExpectedFrame = currentFrame;
+				} else if (secondaryExpectedFrame == 0) {
+					secondaryExpectedFrame = currentFrame;
+				}
+				
+				if (priorityExpectedFrame == currentFrame && priorityExpectedPacket == currentPacket && activeScreen == currentScreen) {
+					// Priority screen
+					byte[] dataToWrite = Arrays.copyOfRange(data, 4, data.length);
+					priorityInputStream.write(dataToWrite);
+					priorityExpectedPacket++;
+					
+					if (isLastPacket) {
+						priorityInputStream.markFinishedWriting();
+						priorityImage = ImageIO.read(priorityInputStream.getInputStream());
+						priorityInputStream = new WritableInputStream();
+						if (colorMode != ColorMode.REGULAR) {
+							priorityImage = ColorHotfix.doColorHotfix(priorityImage, colorMode, false);
+						}
+						if(currentScreen == DSScreen.TOP) {
+							priorityImage = Interpolator.scale(priorityImage, intrp, topScale);
+						} else {
+							priorityImage = Interpolator.scale(priorityImage, intrp, bottomScale);
+						}
+						frameBuffer.add(new Frame(currentScreen, priorityImage));
+						priorityImage = null;
+						priorityExpectedFrame = 0;
+	                    priorityExpectedPacket = 0;
+					}
+				} else if (currentScreen == activeScreen) {
+					// Unexpected priority packet or frame
+					priorityInputStream = new WritableInputStream();
+					priorityImage = null;
+					priorityExpectedFrame = 0;
+                    priorityExpectedPacket = 0;
+				} else if(secondaryExpectedPacket == currentPacket) {
+					// Secondary screen
+					byte[] dataToWrite = Arrays.copyOfRange(data, 4, data.length);
+					secondaryInputStream.write(dataToWrite);
+					secondaryExpectedPacket++;
+					
+					if (isLastPacket) {
+						secondaryInputStream.markFinishedWriting();
+						secondaryImage = ImageIO.read(secondaryInputStream.getInputStream());
+						secondaryInputStream = new WritableInputStream();
+						if (colorMode != ColorMode.REGULAR) {
+							secondaryImage = ColorHotfix.doColorHotfix(secondaryImage, colorMode, false);
+						}
+						if(currentScreen == DSScreen.TOP) {
+							secondaryImage = Interpolator.scale(secondaryImage, intrp, topScale);
+						} else {
+							secondaryImage = Interpolator.scale(secondaryImage, intrp, bottomScale);
+						}
+						frameBuffer.add(new Frame(currentScreen, secondaryImage));
+						secondaryImage = null;
+						secondaryExpectedFrame = 0;
+	                    secondaryExpectedPacket = 0;
+					}
+				} else {
+					// Unexpected secondary packet or frame
+					secondaryInputStream = new WritableInputStream();
+					secondaryImage = null;
+					secondaryExpectedFrame = 0;
+                    secondaryExpectedPacket = 0;
+				}
+			} catch (IOException e) {
+				close();
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
--- app/src/main/java/chokistream/NTRClient.java
+++ app/src/main/java/chokistream/NTRClient.java
@@ -20,7 +20,7 @@ public class NTRClient implements StreamingInterface {
 	/**
 	 * Thread used by NTRClient to read and buffer Frames received from the 3DS.
 	 */
-	private NTRUDPThread thread;
+	private ChirunoUDPThread thread;
 	
 	private static final Logger logger = Logger.INSTANCE;
 
@@ -38,41 +38,47 @@ public class NTRClient implements StreamingInterface {
 	 */
 	public NTRClient(String host, int quality, DSScreen screen, int priority, int qos, ColorMode colorMode, int port,
 			double topScale, double bottomScale, InterpolationMode intrp) throws UnknownHostException, IOException, InterruptedException {
+		
 		// Connect to TCP port and set up client
-		Socket client = new Socket(host, port);
-		client.setTcpNoDelay(true);
-		OutputStream out = client.getOutputStream();
-		thread = new NTRUDPThread(screen, colorMode, topScale, bottomScale, intrp);
+		//Socket client = new Socket(host, port);
+		//client.setTcpNoDelay(true);
+		//OutputStream out = client.getOutputStream();
+		
+		thread = new ChirunoUDPThread(screen, colorMode, topScale, bottomScale, intrp);
 		thread.start();
 		
+		logger.log("ChirunoUDPThread thread started.", LogLevel.VERBOSE);
+		
 		// Creates and sends the initialization packet to the 3DS
-		byte[] initializationPacket = new byte[84];
-		initializationPacket[0] = 0x78;
-		initializationPacket[1] = 0x56;
-		initializationPacket[2] = 0x34;
-		initializationPacket[3] = 0x12;
-		initializationPacket[4] = (byte) 0xb8;
-		initializationPacket[5] = 0x0b;
-		initializationPacket[12] = (byte) 0x85;
-		initializationPacket[13] = 0x03;
-		initializationPacket[16] = (byte) priority;
-		initializationPacket[17] = (byte) ((screen == DSScreen.TOP) ? 0x01 : 0x00);
-		initializationPacket[20] = (byte) quality;
+		//byte[] initializationPacket = new byte[84];
+		//initializationPacket[0] = 0x78;
+		//initializationPacket[1] = 0x56;
+		//initializationPacket[2] = 0x34;
+		//initializationPacket[3] = 0x12;
+		//initializationPacket[4] = (byte) 0xb8;
+		//initializationPacket[5] = 0x0b;
+		//initializationPacket[12] = (byte) 0x85;
+		//initializationPacket[13] = 0x03;
+		//initializationPacket[16] = (byte) priority;
+		//initializationPacket[17] = (byte) ((screen == DSScreen.TOP) ? 0x01 : 0x00);
+		//initializationPacket[20] = (byte) quality;
+		
 		// Nobody has any clue why, but NTR expects double the QoS value
-		initializationPacket[26] = (byte) (qos*2);
-		logger.log("Sending initialization packet", LogLevel.EXTREME);
-		logger.log(initializationPacket, LogLevel.EXTREME);
-		out.write(initializationPacket);
+		
+		//initializationPacket[26] = (byte) (qos*2);
+		//logger.log("Sending initialization packet", LogLevel.EXTREME);
+		//logger.log(initializationPacket, LogLevel.EXTREME);
+		//out.write(initializationPacket);
 		
 		// NTR expects us to disconnect
-		client.close();
+		//client.close();
 		
 		// Give NTR some time to think
 		TimeUnit.SECONDS.sleep(3);
 		
 		// NTR expects us to reconnect, so we will. And then disconnect again!
-		client = new Socket(host, 8000);
-		client.close();
+		//client = new Socket(host, 8000);
+		//client.close();
 	}
 
 	@Override
