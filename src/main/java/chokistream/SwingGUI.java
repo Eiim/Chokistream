9f5db240bbdfdefce353cd96f9f6f3a5f5e32207
Ethan Chapman <ethan@we3chapmans.com>
2023-07-25 22:08:27 -0400

    More visuals of main, about
    Also explicitly sets Java 17

--- src/main/java/chokistream/SwingGUI.java
+++ src/main/java/chokistream/SwingGUI.java
@@ -1,11 +1,16 @@
 package chokistream;
 
 import java.awt.Component;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -26,7 +31,6 @@ import chokistream.props.LogMode;
 import chokistream.props.Mod;
 import chokistream.props.OutputFormat;
 import chokistream.props.Prop;
-import chokistream.props.VideoFormat;
 
 public class SwingGUI extends SettingsUI {
 	
@@ -50,6 +54,18 @@ public class SwingGUI extends SettingsUI {
 	
 	private static final Logger logger = Logger.INSTANCE;
 	
+	private static final String ABOUT_TEXT = "<html>Made by Eiim, herronjo, and ChainSwordCS.<br>"
+								+ "<br>"
+								+ "This software and its source code are provided under the GPL-3.0 License.  See LICENSE for the full license.<br>"
+								+ "<br>"
+								+ "Chokistream was made possible by the use and reference of several projects. Special thanks to:<br>"
+								+ " * RattletraPM for Snickerstream<br>"
+								+ " * Sono for HzMod<br>"
+								+ " * Cell9/44670 for BootNTR<br>"
+								+ " * Nanquitas for BootNTRSelector<br>"
+								+ " * toolboc for UWPStreamer<br>"
+								+ " * All other open-source contributors";
+	
 	public SwingGUI() {
 		FlatLightLaf.setup();
 		f = new JFrame();
@@ -61,7 +77,6 @@ public class SwingGUI extends SettingsUI {
 		JPanel p = new JPanel();
 		p.setBorder(BorderFactory.createEmptyBorder(5, 7, 5, 7));
 		f.add(p);
-		
 		p.setLayout(new GridBagLayout());
 		
 		GridBagConstraints c = new GridBagConstraints();
@@ -101,12 +116,12 @@ public class SwingGUI extends SettingsUI {
 		add(new JLabel(Prop.LOGLEVEL.getLongName()), p, c, 3, 3);
 		add(new JLabel(Prop.LOGFILE.getLongName()), p, c, 3, 4);
 		add(new JLabel(Prop.OUTPUTFORMAT.getLongName()), p, c, 3, 5);
-		add(new JLabel(Prop.VIDEOCODEC.getLongName()), p, c, 3, 6);
-		add(new JLabel(Prop.VIDEOFILE.getLongName()), p, c, 3, 7);
+		//add(new JLabel(Prop.VIDEOCODEC.getLongName()), p, c, 3, 6);
+		//add(new JLabel(Prop.VIDEOFILE.getLongName()), p, c, 3, 7);
 		
 		colorMode = new JComboBox<String>(EnumProp.getLongNames(ColorMode.class));
 		add(colorMode, p, c, 4, 0);
-		port = new JTextField(Prop.IP.getDefault());
+		port = new JTextField(Prop.PORT.getDefault().toString());
 		add(port, p, c, 4, 1);
 		logMode = new JComboBox<String>(EnumProp.getLongNames(LogMode.class));
 		add(logMode, p, c, 4, 2);
@@ -116,12 +131,50 @@ public class SwingGUI extends SettingsUI {
 		add(logFile, p, c, 4, 4);
 		outputFormat = new JComboBox<String>(EnumProp.getLongNames(OutputFormat.class));
 		add(outputFormat, p, c, 4, 5);
-		videoCodec = new JComboBox<String>(EnumProp.getLongNames(VideoFormat.class));
-		add(videoCodec, p, c, 4, 6);
-		videoFile = new JTextField(Prop.VIDEOFILE.getDefault());
-		add(videoFile, p, c, 4, 7);
+		//videoCodec = new JComboBox<String>(EnumProp.getLongNames(VideoFormat.class));
+		//add(videoCodec, p, c, 4, 6);
+		//videoFile = new JTextField(Prop.VIDEOFILE.getDefault());
+		//add(videoFile, p, c, 4, 7);
 		
 		add(new JButton("Mod Settings"), p, c, 0, 1, 2, 1);
+		JButton modSettings = new JButton("Mod Settings");
+		JButton outputSettings = new JButton("Output Settings");
+		JButton controls = new JButton("Controls");
+		JButton about = new JButton("About");
+		JButton connect = new JButton("Connect!");
+		add(modSettings, p, c, 0, 1, 2, 1);
+		add(connect, p, c, 0, 8, 2, 1);
+		add(outputSettings, p, c, 3, 6, 2, 1);
+		add(controls, p, c, 3, 7, 2, 1);
+		add(about, p, c, 3, 8, 2, 1);
+		
+		about.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {createAbout();}
+		});
+		
+		f.getRootPane().setDefaultButton(connect);
+		f.pack();
+		f.setVisible(true);
+	}
+	
+	public void createAbout() {
+		JFrame f = new JFrame();
+		f.setResizable(false);
+		f.setIconImage(IconLoader.get64x());
+		f.setTitle("About");
+		
+		JPanel p = new JPanel();
+		p.setBorder(BorderFactory.createEmptyBorder(5, 10, 7, 10));
+		p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+		f.add(p);
+		
+		JLabel header = new JLabel("Chokistream", new ImageIcon(IconLoader.get64x()), JLabel.LEFT);
+		header.setFont(new Font("System", Font.BOLD, 60));
+		p.add(header);
+		
+		JLabel info = new JLabel(ABOUT_TEXT);
+		p.add(info);
 		
 		f.pack();
 		f.setVisible(true);
