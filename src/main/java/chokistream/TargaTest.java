3940c85d2a76056f59c01c6f0e737e636942ef29
Ethan Chapman <ethan@we3chapmans.com>
2023-07-26 16:54:11 -0400

    Add Targa tester

---
+++ src/main/java/chokistream/TargaTest.java
@@ -0,0 +1,32 @@
+package chokistream;
+
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import chokistream.props.DSScreen;
+import chokistream.props.LogLevel;
+import chokistream.props.LogMode;
+
+public class TargaTest {
+
+	public static void main(String[] args) throws IOException {
+		Logger.INSTANCE.init(LogMode.CONSOLE, LogLevel.EXTREME, "");
+		Scanner s = new Scanner(System.in);
+		FileInputStream fis = new FileInputStream(s.nextLine());
+		BufferedImage im = TargaParser.parseBytes(fis.readAllBytes(), DSScreen.BOTTOM, TGAPixelFormat.RGB8);
+		JFrame f = new JFrame();
+		JLabel l = new JLabel(new ImageIcon(im));
+		f.add(l);
+		f.pack();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		f.toFront();
+	}
+
+}
