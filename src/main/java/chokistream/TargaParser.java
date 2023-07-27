3c7b2363fc4f2c1d18a361bd4fd1b2ade4de65c5
ChainSwordCS <chainswordcs@gmail.com>
2023-07-27 19:30:35 -0400

    TargaParser: 24bpp almost works! And major rework of decoding
    The decoding was split into two functions: tgaDecode and tgaTranslate. The former decodes the Targa image from RLE into RAW without modifying the color data, then the latter translates that color data (if needed) and copies it to the BufferedImage 'image'.
    Note: the output of the parseBytes() function is the same format as before; a BufferedImage object.
    
    Long story short: these two functions have to be separate because HzMod's 24bpp Targa images are all mistakenly encoded at 32bpp. :(

--- src/main/java/chokistream/TargaParser.java
+++ src/main/java/chokistream/TargaParser.java
@@ -72,10 +72,13 @@ public class TargaParser {
 				break;
 		}
 		
+		boolean isMalformed24bpp = false;
+		
 		if(!tgaBppErr) {
 			if(tgaReportedFormat != format) {
 				if(tgaReportedFormat == TGAPixelFormat.RGBA8 && format == TGAPixelFormat.RGB8) {
 					logger.log("Warning: Format is RGBA8 (32bpp) but MODESET packet mis-reports RGB8 (24bpp). This is usually safe to ignore.", LogLevel.EXTREME);
+					isMalformed24bpp = true;
 				}
 				logger.log("Warning: Color format specified in Targa metadata ("+tgaReportedFormat+") differs from format specified by MODESET packet ("+format+")");
 				format = tgaReportedFormat;
@@ -97,6 +100,22 @@ public class TargaParser {
 		logger.log("idfieldlength="+(idfieldlength), LogLevel.EXTREME);
 		int endOfImgDataOffset = data.length - 26; // footer (26 bytes) + other areas (which probably aren't present)
 		
+		
+		if(isMalformed24bpp) {
+			format = TGAPixelFormat.RGBA8;
+		}
+		
+		byte[] decBuf = new byte[400 * 256 * 4]; // middle-man decode buffer
+		decBuf = tgaDecode(data, decBuf, format, width, height, startingoffset, endOfImgDataOffset);
+		
+		if(isMalformed24bpp) {
+			format = TGAPixelFormat.RGB8;
+		}
+		
+		image = tgaTranslate(decBuf, image, format, width, height);
+		
+
+		/*
 		int pxnum = 0;
 		for(int i = startingoffset; i < endOfImgDataOffset && pxnum < width*height;) {
 			boolean errorout = false;
@@ -192,6 +211,178 @@ public class TargaParser {
 				} else {
 					logger.log("Cont.: Unknown error. pxnum="+pxnum+"; width*height="+(width*height)+"; i="+i+"; data.length="+endOfImgDataOffset);
 				}
+				break;
+			}
+		}
+		*/
+		
+		
+		return image;
+	}
+	
+	private static byte[] tgaDecode(byte[] data, byte[] decBuf, TGAPixelFormat format, int width, int height, int startingoffset, int endOfImgDataOffset) {
+		int pxnum = 0;
+		for(int i = startingoffset; i < endOfImgDataOffset && pxnum < width*height;) {
+			boolean errorout = false;
+			byte header = data[i];
+			boolean rle = (header & 0x80) == 0x80; // Top bit is one
+			int packlen = (header & 0x7F) + 1; // Bottom 15 bits plus one
+			
+			if(rle) {
+				i += 1;
+				byte[] colorDat = new byte[format.bytes];
+				for(int k = 0; k < format.bytes; k++) {
+					try {
+						colorDat[k] = data[i+k];
+					} catch(ArrayIndexOutOfBoundsException e) {
+						colorDat[k] = 0;
+						logger.log("Error: Reached end of image data while decoding colors of RLE packet. ("+e.getMessage()+")");
+						// fill the rest of the colors with 0. draw pixels (unless all colors are zero). then break out of the larger for-loop.
+						errorout = true;
+						break;
+					}
+				}
+				
+				/*
+				if(errorout == true && r == 0 && g == 0 && b == 0) {
+					break;
+				}
+				*/
+				
+				// Repeat for as many times as are in packlen
+				for(int pn = 0; pn < packlen; pn++) {
+					for(int k = 0; k < format.bytes; k++) {
+						// Maybe should double-check that we haven't overrun here
+						try {
+							decBuf[(pn*format.bytes)+k] = colorDat[k];
+						} catch(ArrayIndexOutOfBoundsException e) {
+							logger.log("Error: Reached end of image buffer while writing pixels of RLE packet. ("+e.getMessage()+")");
+							// break out of the larger for-loop.
+							errorout = true;
+							break;
+						}
+					}
+					if(errorout) {
+						break; // lazy
+					}
+					pxnum++;
+				}
+				i += format.bytes;
+			} else {
+				i += 1;
+				for(int j = 0; j < packlen; j++) {
+					byte[] colorDat = new byte[format.bytes];
+					for(int k = 0; k < format.bytes; k++) {
+						try {
+							colorDat[k] = data[i+k];
+						} catch(ArrayIndexOutOfBoundsException e) {
+							logger.log("Error: Reached end of image data while decoding pixels of RAW packet. ("+e.getMessage()+")");
+							// fill the rest of the colors with 0. draw this pixel (unless all colors are zero). then break out of the larger for-loop.
+							errorout = true;
+							while(k < format.bytes) {
+								colorDat[k] = 0;
+								k++;
+							}
+							break;
+						}
+					}
+					
+					/*
+					if(errorout == true && r == 0 && g == 0 && b == 0) {
+						break;
+					}
+					*/
+					
+					for(int k = 0; k < format.bytes; k++) {
+						try {
+							decBuf[(pxnum*format.bytes)+k] = colorDat[k];
+						} catch(ArrayIndexOutOfBoundsException e) {
+							logger.log("Error: Reached end of image buffer while writing pixels of RAW packet. ("+e.getMessage()+")");
+							// break out of the larger for-loop.
+							errorout = true;
+							break;
+						}
+					}
+					
+					pxnum++;
+					i += format.bytes;
+					if(errorout == true) {
+						break;
+					}
+				}
+			}
+			
+			if(errorout == true) {
+				if(pxnum >= width*height) {
+					logger.log("Cont.: Received image data exceeds expected size by about "+(endOfImgDataOffset - i)+" bytes. (Is bit-depth mismatched?)");
+				} else if(i >= endOfImgDataOffset) {
+					logger.log("Cont.: Received image data is about "+(width*height - pxnum)+" pixels smaller than expected. (Is bit-depth mismatched?)");
+				} else {
+					logger.log("Cont.: Unknown error. pxnum="+pxnum+"; width*height="+(width*height)+"; i="+i+"; data.length="+endOfImgDataOffset);
+				}
+				break;
+			}
+		}
+		return decBuf;
+	}
+	
+	private static BufferedImage tgaTranslate(byte[] decBuf, BufferedImage image, TGAPixelFormat format, int width, int height) {
+		int pxnum = 0;
+		for(int i = 0; i < (400*256*4) && pxnum < width*height;) {
+			boolean errorout = false;
+			
+			// no headers; all RAW
+			
+			int[] colorDat = new int[format.bytes];
+			
+			for(int k = 0; k < format.bytes; k++) {
+				try {
+					colorDat[k] = decBuf[i+k] & 0xff;
+				} catch(ArrayIndexOutOfBoundsException e) {
+					logger.log("Error: Reached end of image data while decoding pixels of RAW packet. ("+e.getMessage()+")");
+					// fill the rest of the colors with 0. draw this pixel (unless all colors are zero). then break out of the larger for-loop.
+					errorout = true;
+					while(k < format.bytes) {
+						colorDat[k] = 0;
+						k++;
+					}
+					break;
+				}
+			}
+			int[] rgb = getRGB(colorDat, format);
+			int r = rgb[0];
+			int g = rgb[1];
+			int b = rgb[2];
+			
+			if(errorout == true && r == 0 && g == 0 && b == 0) {
+				break;
+			}
+			
+			try {
+				image.setRGB(pxnum%width, pxnum/width, (r << 16) | (g << 8) | b);
+			} catch(ArrayIndexOutOfBoundsException e) {
+				logger.log("Error: Reached end of image buffer while writing pixels of RAW packet. ("+e.getMessage()+")");
+				// break out of the larger for-loop.
+				errorout = true;
+				break;
+			}
+			pxnum++;
+			i += format.bytes;
+			if(errorout == true) {
+				break;
+			}
+			
+			if(errorout == true) {
+				//TODO: fix or rework
+				/*
+				if(pxnum >= width*height) {
+					logger.log("Cont.: Received image data exceeds expected size by about "+(endOfImgDataOffset - i)+" bytes. (Is bit-depth mismatched?)");
+				} else if(i >= endOfImgDataOffset) {
+					logger.log("Cont.: Received image data is about "+(width*height - pxnum)+" pixels smaller than expected. (Is bit-depth mismatched?)");
+				} else {
+					logger.log("Cont.: Unknown error. pxnum="+pxnum+"; width*height="+(width*height)+"; i="+i+"; data.length="+endOfImgDataOffset);
+				}
+				*/
 				break;
 			}
 		}
