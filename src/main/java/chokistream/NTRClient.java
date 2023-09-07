d927418b4cdab25b0bf7d7af72cda5ecc95dd60d
ChainSwordCS <chainswordcs@gmail.com>
2023-09-07 19:36:26 -0400

    Add license info in NTRClient.java

--- src/main/java/chokistream/NTRClient.java
+++ src/main/java/chokistream/NTRClient.java
@@ -1,3 +1,23 @@
+/**
+ * NTRClient.java
+ * Copyright (C) 2023  Eiim, ChainSwordCS, Herronjo
+ * Some code is based on NTRClient, which is Copyright (C) 2016  Cell9 / 44670
+ * 
+ * This file is licensed under GPLv2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package chokistream;
 
 import java.io.IOException;
