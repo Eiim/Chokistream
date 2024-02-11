Eiim/Chokistream

commit e6c065bd6e1c7b9ed611dd538808794d67567ee3

Performance improvements for scaling and Hz/CHM
* Use lower-level Raster API for upscaling
* Rewrite ImageManipulator to avoid hot per-pixel code paths and simplify the API surface for NTR
Between this commit and the last, upscaling should be noticeably faster (~30% faster at 1.5x with my hardware, and increasingly more efficient with larger upscale factors).
Downscaling is slightly less efficient but I doubt that's an issue for anybody and it's not by much.
HzMod and ChirunoMod image manipulation is now around 50% faster. There should now be next to no chance of PC bottlenecks for those mods given their generally lower framerate.

src/main/java/chokistream/ImageManipulator.java
@@ -9,26 +9,23 @@ public class ImageManipulator {
 	private static Logger logger = Logger.INSTANCE;
 	
 	/**
-	 * Rotates and color-adjusts an image
+	 * For NTR: rotates and color-adjusts an image
 	 * @param in Original image
 	 * @param cm Color adjustment mode
-	 * @param swapRB Swap R/B channels (HzM/CHM)
 	 * @return Adjusted image
 	 */
-	public static BufferedImage adjust(BufferedImage in, ColorMode cm, boolean swapRB) {
+	public static BufferedImage adjust(BufferedImage in, ColorMode cm) {
 		BufferedImage out = new BufferedImage(in.getHeight(), in.getWidth(), in.getType());
-		int ow = out.getWidth();
-		int oh = out.getHeight();
-		for(int i = 0; i < ow; i++) {
-			for(int j = 0; j < oh; j++) {
-				out.setRGB(i, oh-j-1, ColorHotfix.hotfixPixel(in.getRGB(j, i), cm, swapRB));
-			}
+		if(cm == ColorMode.REGULAR) {
+			return adjustStandard(out, in, 0);
+		} else {
+			return adjustCM(out, in, 0, cm);
 		}
-		return out;
 	}
 	
 	/**
 	 * Combines an image with another, doing interpolation, offset, color adjustment, and rotation, as necessary.
+	 * Propagates down to optimized code paths.
 	 * @param base Image to be combined onto
 	 * @param in Image to combine
 	 * @param interlace Whether or not to interlace
@@ -38,21 +35,150 @@ public class ImageManipulator {
 	 * @param swapRB Swap R/B channels (HzM/CHM)
 	 * @return Resultant image
 	 */
-	public static BufferedImage adjust(BufferedImage base, BufferedImage in, boolean interlace, int interParity, int offset, ColorMode cm, boolean swapRB) throws ArrayIndexOutOfBoundsException {
-		//logger.log(String.format("Composing %d x %d onto %d x %d with i=%s,%d, o=%d, cm=%s, and rb=%s", in.getWidth(), in.getHeight(), base.getWidth(), base.getHeight(), interlace, interParity, offset, cm.getLongName(), swapRB), LogLevel.VERBOSE);
+	public static BufferedImage adjust(BufferedImage base, BufferedImage in, boolean interlace, int interParity, int offset, ColorMode cm, boolean swapRB) {
+		if(interlace) {
+			if(swapRB) {
+				if(cm == ColorMode.REGULAR) {
+					return adjustIntRB(base, in, interParity, offset);
+				} else {
+					return adjustIntCMRB(base, in, interParity, offset, cm);
+				}
+			} else {
+				if(cm == ColorMode.REGULAR) {
+					return adjustInt(base, in, interParity, offset);
+				} else {
+					return adjustIntCM(base, in, interParity, offset, cm);
+				}
+			}
+		} else {
+			if(swapRB) {
+				if(cm == ColorMode.REGULAR) {
+					return adjustRB(base, in, offset);
+				} else {
+					return adjustCMRB(base, in, offset, cm);
+				}
+			} else {
+				if(cm == ColorMode.REGULAR) {
+					return adjustStandard(base, in, offset);
+				} else {
+					return adjustCM(base, in, offset, cm);
+				}
+			}
+		}
+	}
+	
+	// Only offset + rotation
+	private static BufferedImage adjustStandard(BufferedImage base, BufferedImage in, int offset) {
+		int ow = base.getWidth();
+		int oh = base.getHeight();
+		for(int i = 0; i < ow; i++) {
+			for(int j = 0; j < oh; j++) {
+				base.setRGB(i + offset, oh-j-1, in.getRGB(j, i));
+			}
+		}
+		return base;
+	}
+	
+	// interlace + offset + rotation
+	private static BufferedImage adjustInt(BufferedImage base, BufferedImage in, int interParity, int offset) {
 		int iw = in.getWidth();
 		int ih = in.getHeight();
-		int oh = base.getHeight();
 		for(int i = 0; i < iw; i++) {
 			for(int j = 0; j < ih; j++) {
-				int oy = interlace ? oh-(2*i)-interParity-1 : oh-i-1; // Maybe should do branchless somehow? eh
 				try {
-					//logger.logOnce(in.getRGB(i, j)+"");
-					base.setRGB(j + offset, oy, ColorHotfix.hotfixPixel(in.getRGB(i, j), cm, swapRB));
+					base.setRGB(j + offset, iw-(2*i)-interParity-1, in.getRGB(i, j));
 				} catch(ArrayIndexOutOfBoundsException e) {
-					logger.log(i+" "+j+" "+(j+offset)+" "+oy);
+					logger.log(i+" "+j+" "+(j+offset)+" "+(iw-(2*i)-interParity-1));
 					throw e;
 				}
+			}
+		}
+		return base;
+	}
+	
+	// interlace + offset + rotation + colormode
+	private static BufferedImage adjustIntCM(BufferedImage base, BufferedImage in, int interParity, int offset, ColorMode cm) {
+		int iw = in.getWidth();
+		int ih = in.getHeight();
+		for(int i = 0; i < iw; i++) {
+			for(int j = 0; j < ih; j++) {
+				try {
+					base.setRGB(j + offset, iw-(2*i)-interParity-1, ColorHotfix.hotfixPixel(in.getRGB(i, j), cm));
+				} catch(ArrayIndexOutOfBoundsException e) {
+					logger.log(i+" "+j+" "+(j+offset)+" "+(iw-(2*i)-interParity-1));
+					throw e;
+				}
+			}
+		}
+		return base;
+	}
+	
+	// interlace + offset + rotation + swap rb
+	private static BufferedImage adjustIntRB(BufferedImage base, BufferedImage in, int interParity, int offset) {
+		int iw = in.getWidth();
+		int ih = in.getHeight();
+		for(int i = 0; i < iw; i++) {
+			for(int j = 0; j < ih; j++) {
+				int oy = iw-(2*i)-interParity-1;
+				try {
+					base.setRGB(j + offset, iw-(2*i)-interParity-1, ColorHotfix.hzModSwapRedBlue(in.getRGB(i, j)));
+				} catch(ArrayIndexOutOfBoundsException e) {
+					logger.log(i+" "+j+" "+(j+offset)+" "+(iw-(2*i)-interParity-1));
+					throw e;
+				}
+			}
+		}
+		return base;
+	}
+	
+	// interlace + offset + rotation + swap rb + colormode
+	private static BufferedImage adjustIntCMRB(BufferedImage base, BufferedImage in, int interParity, int offset, ColorMode cm) {
+		int iw = in.getWidth();
+		int ih = in.getHeight();
+		for(int i = 0; i < iw; i++) {
+			for(int j = 0; j < ih; j++) {
+				try {
+					base.setRGB(j + offset, iw-(2*i)-interParity-1, ColorHotfix.hotfixPixel(ColorHotfix.hzModSwapRedBlue(in.getRGB(i, j)), cm));
+				} catch(ArrayIndexOutOfBoundsException e) {
+					logger.log(i+" "+j+" "+(j+offset)+" "+(iw-(2*i)-interParity-1));
+					throw e;
+				}
+			}
+		}
+		return base;
+	}
+	
+	// offset + rotation + colormode
+	private static BufferedImage adjustCM(BufferedImage base, BufferedImage in, int offset, ColorMode cm) {
+		int iw = in.getWidth();
+		int ih = in.getHeight();
+		for(int i = 0; i < iw; i++) {
+			for(int j = 0; j < ih; j++) {
+				base.setRGB(j + offset, iw-i-1, ColorHotfix.hotfixPixel(in.getRGB(i, j), cm));
+			}
+		}
+		return base;
+	}
+	
+	// offset + rotation + swap rb
+	private static BufferedImage adjustRB(BufferedImage base, BufferedImage in, int offset) {
+		int iw = in.getWidth();
+		int ih = in.getHeight();
+		for(int i = 0; i < iw; i++) {
+			for(int j = 0; j < ih; j++) {
+				base.setRGB(j + offset, iw-i-1, ColorHotfix.hzModSwapRedBlue(in.getRGB(i, j)));
+			}
+		}
+		return base;
+	}
+	
+	// offset + rotation + swap rb + colormode
+	private static BufferedImage adjustCMRB(BufferedImage base, BufferedImage in, int offset, ColorMode cm) {
+		int iw = in.getWidth();
+		int ih = in.getHeight();
+		for(int i = 0; i < iw; i++) {
+			for(int j = 0; j < ih; j++) {
+				base.setRGB(j + offset, iw-i-1, ColorHotfix.hotfixPixel(ColorHotfix.hzModSwapRedBlue(in.getRGB(i, j)), cm));
 			}
 		}
 		return base;

.settings/org.eclipse.buildship.core.prefs
@@ -1,2 +1,13 @@
+arguments=
+auto.sync=false
+build.scans.enabled=false
+connection.gradle.distribution=GRADLE_DISTRIBUTION(WRAPPER)
 connection.project.dir=
 eclipse.preferences.version=1
+gradle.user.home=
+java.home=
+jvm.arguments=
+offline.mode=false
+override.workspace.settings=false
+show.console.view=false
+show.executions.view=false

src/main/java/chokistream/ColorHotfix.java
@@ -1,12 +1,10 @@
 package chokistream;
 
-import java.awt.image.BufferedImage;
-
 import chokistream.props.ColorMode;
 
 public class ColorHotfix {
 	
-	private static int hzModSwapRedBlue(int currentPixelColor) {
+	public static int hzModSwapRedBlue(int currentPixelColor) {
 		// Assume full alpha because it always should be
 		//int newAlphaIdk = ((currentPixelColor&0b11111111000000000000000000000000) >>> 24);
 		int newRedPixel = ((currentPixelColor&0b00000000000000000000000011111111)); //bitwise AND
@@ -15,7 +13,7 @@ public class ColorHotfix {
 		return 0xFF000000 +  ((newRedPixel)<<16) + ((newGrnPixel)<<8) + (newBluPixel);
 	}
 	
-	private static int makeGrayscale(int currentPixelColor) {
+	public static int makeGrayscale(int currentPixelColor) {
 		int newRedPixel = ((currentPixelColor&0b00000000111111110000000000000000) >>> 16);
 		int newGrnPixel = ((currentPixelColor&0b00000000000000001111111100000000) >>> 8);
 		int newBluPixel = ((currentPixelColor&0b00000000000000000000000011111111));
@@ -24,7 +22,7 @@ public class ColorHotfix {
 	}
 	
 	// THIS IS BROKEN. IT IS BORDERLINE UNSOLVABLE. TRUST ME, I SPENT HOURS ON THIS. -C
-	private static int vcBlueShift(int currentPixelColor) {
+	public static int vcBlueShift(int currentPixelColor) {
 		//int newAlphaIdk = ((currentPixelColor&0b11111111000000000000000000000000) >>> 24);
 		int newRedPixel = ((currentPixelColor&0b00000000111111110000000000000000) >>> 16);
 		int newGrnPixel = ((currentPixelColor&0b00000000000000001111111100000000) >>> 8);
@@ -95,27 +93,6 @@ public class ColorHotfix {
 	 * newBluPixel = (int)((newBluPixel/256.0) * 0x48) + newBluPixel;
 	 */
 	
-	public static BufferedImage doColorHotfix(BufferedImage hotfixImage, ColorMode colorMode, boolean swapRB) {
-		try {
-			int currentPixelColor = 0;
-			int currentW = 0;
-			int currentH = 0;
-			while (currentH < hotfixImage.getHeight()) {
-				while (currentW < hotfixImage.getWidth()) {
-					currentPixelColor = hotfixImage.getRGB(currentW, currentH);
-					hotfixImage.setRGB(currentW, currentH, hotfixPixel(currentPixelColor, colorMode, swapRB));
-					currentW++;
-				}
-				currentW = 0;
-				currentH++;
-			}
-			return hotfixImage;
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-			return new BufferedImage(400, 240, BufferedImage.TYPE_INT_RGB);
-		}
-	}
-	
 	public static int hotfixPixel(int in, ColorMode colorMode, boolean swapRB) {
 		int ret = in;
 		
@@ -126,6 +103,15 @@ public class ColorHotfix {
 			case GRAYSCALE -> makeGrayscale(in);
 			case REGULAR -> ret;
 			case VC_BLUE_SHIFT -> vcBlueShift(in);
+		};
+	}
+	
+	// Probably slower than we'd like, but it should be rarely used
+	public static int hotfixPixel(int in, ColorMode colorMode) {
+		return switch(colorMode) {
+			case GRAYSCALE -> makeGrayscale(in);
+			case VC_BLUE_SHIFT -> vcBlueShift(in);
+			case REGULAR -> in;
 		};
 	}
 	

src/main/java/chokistream/HZModClient.java
@@ -246,22 +246,16 @@ public class HZModClient implements StreamingInterface {
 		}
 		
 		if(screen == DSScreen.BOTTOM) {
-			if(image.getHeight() == 320) { // Full screen
-				bottomFrames++;
-				lastBottomImage = ImageManipulator.adjust(image, colorMode, rbSwap);
-			} else if(image.getWidth() > 1) { // Fractional
-				if(image.getHeight() + offset == 320) bottomFrames++;
-				lastBottomImage = ImageManipulator.adjust(lastBottomImage, image, false, 0, offset, colorMode, rbSwap);;
+			if(image.getWidth() > 1) {
+				if(image.getHeight() + offset == 320) bottomFrames++; // works for both fractional and non-fractional
+				lastBottomImage = ImageManipulator.adjust(lastBottomImage, image, false, 0, offset, colorMode, rbSwap);
 			} else {
 				// 1-wide frame, use the last one instead
 			}
 			returnFrame = new Frame(DSScreen.BOTTOM, lastBottomImage);
 		} else {
-			if(image.getHeight() == 400) { // Full screen
-				topFrames++;
-				lastTopImage = ImageManipulator.adjust(image, colorMode, rbSwap);
-			} else if(image.getWidth() > 1) { // Fractional
-				if(image.getHeight() + offset == 400) topFrames++;
+			if(image.getWidth() > 1) {
+				if(image.getHeight() + offset == 400) topFrames++; // works for both fractional and non-fractional
 				lastTopImage = ImageManipulator.adjust(lastTopImage, image, false, 0, offset, colorMode, rbSwap);
 			} else {
 				// 1-wide frame, use the last one instead

src/main/java/chokistream/Interpolator.java
@@ -1,6 +1,7 @@
 package chokistream;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 
 import chokistream.props.InterpolationMode;
 
@@ -19,19 +20,24 @@ public class Interpolator {
 		if(in == null) {
 			return new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
 		}
+		
+		WritableRaster r = in.getRaster();
 		int oldW = in.getWidth();
 		int oldH = in.getHeight();
-		BufferedImage out = new BufferedImage((int)(scale*oldW), (int)(scale*oldH), in.getType());
+		WritableRaster out = r.createCompatibleWritableRaster((int)(scale*oldW), (int)(scale*oldH));
+		int[] storage = new int[r.getSampleModel().getNumBands()];
+		
 		for(int i = 0; i < oldW; i++) {
 			for(int j = 0; j < oldH; j++) {
-				int rgb = in.getRGB(i, j);
+				int[] pixel = r.getPixel(i, j, storage);
 				for(int i2 = (int)(scale*i); i2 < (int)(scale*(i+1)); i2++) {
 					for(int j2 = (int)(scale*j); j2 < (int)(scale*(j+1)); j2++) {
-						out.setRGB(i2, j2, rgb);
+						out.setPixel(i2, j2, pixel);
 					}
 				}
 			}
 		}
-		return out;
+		
+		return new BufferedImage(in.getColorModel(), out, in.isAlphaPremultiplied(), null); // I assume we never have any properties? or if we do they're not needed
 	}
 }

src/main/java/chokistream/NTRUDPThread.java
@@ -110,7 +110,7 @@ public class NTRUDPThread extends Thread {
 						priorityImage = ImageIO.read(new ByteArrayInputStream(priorityImageData));
 						priorityImageData = new byte[0];
 						
-						priorityImage = ImageManipulator.adjust(priorityImage, colorMode, false);
+						priorityImage = ImageManipulator.adjust(priorityImage, colorMode);
 						
 						frameBuffer.poll();
 						frameBuffer.add(new Frame(currentScreen, priorityImage));
@@ -141,7 +141,7 @@ public class NTRUDPThread extends Thread {
 						secondaryImage = ImageIO.read(new ByteArrayInputStream(secondaryImageData));
 						secondaryImageData = new byte[0];
 						
-						secondaryImage = ImageManipulator.adjust(secondaryImage, colorMode, false);
+						secondaryImage = ImageManipulator.adjust(secondaryImage, colorMode);
 						
 						frameBuffer.poll();
 						frameBuffer.add(new Frame(currentScreen, secondaryImage));

commit ebaa8f1254b5be21e458fe5e24d121f35947edee

Fixed interlacing behavior in ImageManipulator.java
Fixed a regression from e6c065b

src/main/java/chokistream/ImageManipulator.java
@@ -24,7 +24,7 @@ public class ImageManipulator {
 	}
 	
 	/**
-	 * Combines an image with another, doing interpolation, offset, color adjustment, and rotation, as necessary.
+	 * Combines an image with another, doing interlacing, offset, color adjustment, and rotation, as necessary.
 	 * Propagates down to optimized code paths.
 	 * @param base Image to be combined onto
 	 * @param in Image to combine
@@ -88,7 +88,7 @@ public class ImageManipulator {
 		for(int i = 0; i < iw; i++) {
 			for(int j = 0; j < ih; j++) {
 				try {
-					base.setRGB(j + offset, iw-(2*i)-interParity-1, in.getRGB(i, j));
+					base.setRGB(j + offset, 2*(iw-i)-interParity-1, in.getRGB(i, j));
 				} catch(ArrayIndexOutOfBoundsException e) {
 					logger.log(i+" "+j+" "+(j+offset)+" "+(iw-(2*i)-interParity-1));
 					throw e;
@@ -105,7 +105,7 @@ public class ImageManipulator {
 		for(int i = 0; i < iw; i++) {
 			for(int j = 0; j < ih; j++) {
 				try {
-					base.setRGB(j + offset, iw-(2*i)-interParity-1, ColorHotfix.hotfixPixel(in.getRGB(i, j), cm));
+					base.setRGB(j + offset, 2*(iw-i)-interParity-1, ColorHotfix.hotfixPixel(in.getRGB(i, j), cm));
 				} catch(ArrayIndexOutOfBoundsException e) {
 					logger.log(i+" "+j+" "+(j+offset)+" "+(iw-(2*i)-interParity-1));
 					throw e;
@@ -122,7 +122,7 @@ public class ImageManipulator {
 		for(int i = 0; i < iw; i++) {
 			for(int j = 0; j < ih; j++) {
 				try {
-					base.setRGB(j + offset, iw-(2*i)-interParity-1, ColorHotfix.hzModSwapRedBlue(in.getRGB(i, j)));
+					base.setRGB(j + offset, 2*(iw-i)-interParity-1, ColorHotfix.hzModSwapRedBlue(in.getRGB(i, j)));
 				} catch(ArrayIndexOutOfBoundsException e) {
 					logger.log(i+" "+j+" "+(j+offset)+" "+(iw-(2*i)-interParity-1));
 					throw e;
@@ -139,7 +139,7 @@ public class ImageManipulator {
 		for(int i = 0; i < iw; i++) {
 			for(int j = 0; j < ih; j++) {
 				try {
-					base.setRGB(j + offset, iw-(2*i)-interParity-1, ColorHotfix.hotfixPixel(ColorHotfix.hzModSwapRedBlue(in.getRGB(i, j)), cm));
+					base.setRGB(j + offset, 2*(iw-i)-interParity-1, ColorHotfix.hotfixPixel(ColorHotfix.hzModSwapRedBlue(in.getRGB(i, j)), cm));
 				} catch(ArrayIndexOutOfBoundsException e) {
 					logger.log(i+" "+j+" "+(j+offset)+" "+(iw-(2*i)-interParity-1));
 					throw e;