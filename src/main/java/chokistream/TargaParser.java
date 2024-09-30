0bf44d30075125dc3a60cafba8c21ea9801f7505
ChainSwordCS <chainswordcs@gmail.com>
2024-09-29 20:05:00 -0400

    TargaParser: Added support for RLE colormapped images
    Could use some code cleanup, but as a quick initial implementation it works.

--- src/main/java/chokistream/TargaParser.java
+++ src/main/java/chokistream/TargaParser.java
@@ -1,7 +1,9 @@
 package chokistream;
 
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import chokistream.props.DSScreen;
 import chokistream.props.LogLevel;
@@ -32,8 +34,9 @@ public class TargaParser {
 		byte imageType = data[2];
 		if(colorMapPresent) {
 			switch(imageType) {
+				case 9:
+					//doRleDecode = true;
 				case 1:
-				//case 9:
 					doColorMapDecode = true;
 					break;
 				default:
@@ -113,24 +116,29 @@ public class TargaParser {
 		int footerOffs = data.length - 26;
 		
 		if(doColorMapDecode) {
+			// adjust "endOfImgData" to account for existence of colormap
 			int colorMapOffs = startOfImgData;
-			startOfImgData = tgaReportedFormat.bytes * colorMapLength;
-			image = tgaDecodeRawColorMappedImg(data, image, tgaReportedFormat, width, height, startOfImgData, colorMapOffs, colorMapLength);
+			startOfImgData += tgaReportedFormat.bytes * colorMapLength;
+			// decode
+			byte[] decBuf = new byte[width*height];
+			decBuf = tgaDecompressRLEImg(data, decBuf, 1, width, height, startOfImgData, footerOffs);
+			byte[] palette = Arrays.copyOfRange(data, colorMapOffs, colorMapOffs+tgaReportedFormat.bytes*colorMapLength);
+			image = tgaTranslateColorMappedImg(decBuf, image, tgaReportedFormat, width, height, 0, palette, colorMapLength);
 		} else {
 			byte[] decBuf = new byte[400 * 256 * 4]; // middle-man decode buffer
-			decBuf = tgaDecode(data, decBuf, tgaReportedFormat, width, height, startOfImgData, footerOffs);
+			decBuf = tgaDecompressRLEImg(data, decBuf, tgaReportedFormat.bytes, width, height, startOfImgData, footerOffs);
 			
 			// Workaround known HzMod bug; 3DS-side, 24bpp images are encoded as if they were 32bpp. Refer to docs.
 			if(isMalformed24bpp) {
 				tgaReportedFormat = TGAPixelFormat.RGB8;
 			}
 			
-			image = tgaTranslate(decBuf, image, tgaReportedFormat, width, height);
+			image = tgaTranslateRawImg(decBuf, image, tgaReportedFormat, width, height);
 		}
 		return image;
 	}
 	
-	private static byte[] tgaDecode(byte[] data, byte[] decBuf, TGAPixelFormat format, int width, int height, int startOfImgData, int endOfImgData) {
+	private static byte[] tgaDecompressRLEImg(byte[] data, byte[] decBuf, int bytesPerPx, int width, int height, int startOfImgData, int endOfImgData) {
 		if(endOfImgData >= data.length || startOfImgData < 0) {
 			throw new IllegalArgumentException("Data boundaries of "+startOfImgData+"-"+endOfImgData+" don't make sense for data with length "+data.length+"!");
 		}
@@ -141,66 +149,73 @@ public class TargaParser {
 		int i = startOfImgData;
 		
 		packetloop:
-		while(i < endOfImgData && pxnum < width*height && !errorEndOfInput && !errorEndOfDecbuf) { // do we need both of the first two checks?
+		while(pxnum < width*height) {
+			if(errorEndOfInput || errorEndOfDecbuf)
+				break;
+			if(i >= endOfImgData)
+			{
+				errorEndOfInput = true;
+				break;
+			}
 			byte header = data[i];
 			boolean rle = (header & 0b10000000) > 0; // Top bit is one
-			int packlen = (header & 0b01111111) + 1; // Bottom 7 bits plus one
+			int packlen = ((header & 0b01111111) + 1) & 0xFF; // Bottom 7 bits plus one
 			i += 1;
 			
 			if(rle) {
-				byte[] colorDat = Arrays.copyOfRange(data, i, i+format.bytes); // Automatically fills any extra positions with 0
-				if(i+format.bytes > endOfImgData) {
-					logger.log("Error: Reached end of image data while decoding colors of RLE packet. [tgaDecode()]");
+				byte[] colorDat = Arrays.copyOfRange(data, i, i+bytesPerPx); // Automatically fills any extra positions with 0
+				if(i+bytesPerPx > endOfImgData) {
+					logger.log("Error: Reached end of input image data while decoding colors of RLE packet. [tgaDecompressRLEImg()]");
 					errorEndOfInput = true; // Will want to break out of the loop later since we hit the end, but first encode the pixels
 				}
 				
 				// Repeat for each pixel we're encoding
 				for(int j = 0; j < packlen; j++) {
-					if((pxnum+j+1)*format.bytes > decBuf.length) {
+					if((pxnum+j+1)*bytesPerPx > decBuf.length) {
 						// We're going to overrun. Copy as much as possible, although this will result in broken color data
-						System.arraycopy(colorDat, 0, decBuf, (pxnum+j)*format.bytes, decBuf.length-(pxnum+j)*format.bytes-1);
-						logger.log("Error: Reached end of image buffer while writing pixels of RLE packet. [tgaDecode()] ("+((pxnum+j+1)*format.bytes)+">"+decBuf.length+")");
+						System.arraycopy(colorDat, 0, decBuf, (pxnum+j)*bytesPerPx, decBuf.length-(pxnum+j)*bytesPerPx-1);
+						logger.log("Error: Reached end of output image buffer while writing pixels of RLE packet. [tgaDecompressRLEImg()] ("+((pxnum+j+1)*bytesPerPx)+">"+decBuf.length+")");
 						// break out of the larger while-loop.
 						pxnum += j;
 						errorEndOfDecbuf = true;
 						break packetloop;
 					} else {
 						// We know it won't overrun here
-						System.arraycopy(colorDat, 0, decBuf, (pxnum+j)*format.bytes, format.bytes);
+						System.arraycopy(colorDat, 0, decBuf, (pxnum+j)*bytesPerPx, bytesPerPx);
 					}
 				}
 				
 				pxnum += packlen;
-				i += format.bytes;
+				i += bytesPerPx;
 			} else {
-				int bytelen = packlen*format.bytes;
+				int bytelen = packlen*bytesPerPx;
 				
 				if(i+bytelen > endOfImgData) {
-					logger.log("Error: Reached end of image data while decoding pixels of RAW packet. [tgaDecode()] (Attempted to read "+bytelen+" bytes starting at "+i+" but data is only "+endOfImgData+" bytes long)");
+					logger.log("Error: Reached end of input image data while decoding pixels of RAW packet. [tgaDecompressRLEImg()] (Attempted to read "+bytelen+" bytes starting at "+i+" but image data ends at "+endOfImgData+" bytes)");
 					// fill the rest of the colors with 0. draw this pixel (unless all colors are zero). then break out of the larger while-loop.
 					bytelen = endOfImgData-i-1;
 					errorEndOfInput = true;
 				}
 				
-				if((pxnum*format.bytes)+bytelen >= decBuf.length) {
-					logger.log("Error: Reached end of image buffer while writing pixels of RAW packet. [tgaDecode()] (Attempted to write "+bytelen+" bytes starting at "+pxnum*format.bytes+" but buffer is only "+decBuf.length+" bytes long)");
+				if((pxnum*bytesPerPx)+bytelen > decBuf.length) {
+					logger.log("Error: Reached end of output image buffer while writing pixels of RAW packet. [tgaDecompressRLEImg()] (Attempted to write "+bytelen+" bytes starting at "+pxnum*bytesPerPx+" but buffer is only "+decBuf.length+" bytes long)");
 					// Write as much data as possible
-					bytelen = decBuf.length - (pxnum*format.bytes) - 1;
+					bytelen = decBuf.length - (pxnum*bytesPerPx) - 1;
 					errorEndOfDecbuf = true;
 				}
 				
 				// Should all be safe now so that we can't get an exception here
-				System.arraycopy(data, i, decBuf, pxnum*format.bytes, bytelen);
+				System.arraycopy(data, i, decBuf, pxnum*bytesPerPx, bytelen);
 				
 				// Fill any extra 0s here
-				if(bytelen%format.bytes != 0) {
-					for(int j = 0; j < format.bytes-(bytelen%format.bytes); j++) {
-						decBuf[(pxnum*format.bytes)+bytelen+j] = 0;
+				if(bytelen%bytesPerPx != 0) {
+					for(int j = 0; j < bytesPerPx-(bytelen%bytesPerPx); j++) {
+						decBuf[(pxnum*bytesPerPx)+bytelen+j] = 0;
 					}
 				}
 				
 				i += bytelen;
-				pxnum += bytelen/format.bytes;
+				pxnum += bytelen/bytesPerPx;
 			}
 		}
 		
@@ -210,11 +225,11 @@ public class TargaParser {
 		if(errorEndOfDecbuf) {
 			logger.log("Cont.: Received image data exceeds expected size by about "+(endOfImgData - i)+" bytes. (Is bit-depth mismatched?)");
 		}
-			
+		
 		return decBuf;
 	}
 	
-	private static BufferedImage tgaTranslate(byte[] decBuf, BufferedImage image, TGAPixelFormat format, int width, int height) {
+	private static BufferedImage tgaTranslateRawImg(byte[] decBuf, BufferedImage image, TGAPixelFormat format, int width, int height) {
 		boolean errorEndOfInput = false;
 		boolean errorEndOfOutputBuffer = false;
 		int pxnum = 0;
@@ -225,14 +240,14 @@ public class TargaParser {
 			
 			byte[] colorDat = Arrays.copyOfRange(decBuf, i, i+format.bytes); // Extra 0s automatically appended if needed
 			if(i+format.bytes >= decBuf.length) {
-				logger.log("Error: Reached end of image data while decoding pixels of RAW packet. [tgaTranslate()] (Attempted to read "+format.bytes+" bytes starting at "+i+" but image data is "+decBuf.length+" bytes long)");
+				logger.log("Error: Reached end of image data while decoding pixels of RAW packet. [tgaTranslateRawImg()] (Attempted to read "+format.bytes+" bytes starting at "+i+" but image data is "+decBuf.length+" bytes long)");
 				errorEndOfInput = true; // Exceeded length of input data, break out of larger loop but still write
 			}
 			
 			try {
 				image.setRGB(pxnum%width, pxnum/width, getRGB(colorDat, format));
 			} catch(ArrayIndexOutOfBoundsException e) {
-				logger.log("Error: Reached end of image buffer while writing pixels of RAW packet. [tgaTranslate()] ("+e.getMessage()+")");
+				logger.log("Error: Reached end of image buffer while writing pixels of RAW packet. [tgaTranslateRawImg()] ("+e.getMessage()+")");
 				// break out of the larger for-loop.
 				errorEndOfOutputBuffer = true;
 				break;
@@ -252,23 +267,46 @@ public class TargaParser {
 		return image;
 	}
 	
-	private static BufferedImage tgaDecodeRawColorMappedImg(byte[] data, BufferedImage image, TGAPixelFormat format, int width, int height, int imgDataOffs, int colorMapDataOffs, int colorMapLength) {
+	private static BufferedImage tgaTranslateColorMappedImg(byte[] decBuf, BufferedImage image, TGAPixelFormat format, int width, int height, int imgDataOffs, byte[] colorMap, int colorMapLength) {
 		
-		
-		int[] colorMap = new int[colorMapLength];
-		for(int i = 0; i < colorMapLength; i ++) {
-			byte[] colorDat = Arrays.copyOfRange(data, colorMapDataOffs+(i*format.bytes), colorMapDataOffs+(i*format.bytes+format.bytes));
-			colorMap[i] = getRGB(colorDat, format);
+		int[] newColorMap = new int[colorMapLength];
+		int i = 0;
+		int j = 0;
+		while(i < colorMapLength) {
+			byte[] colorDat = Arrays.copyOfRange(colorMap, j, j+format.bytes);
+			newColorMap[i] = getRGB(colorDat, format);
+			i++;
+			j += format.bytes;
 		}
 		
-		imgDataOffs += 18; // temp hack
+		List paletteOobErrorIndex = new ArrayList();
+		List paletteOobErrorCount = new ArrayList();
 		
 		for(int pxnum = 0; pxnum < width*height; pxnum++) {
-			try {
-				int colorIndex = data[imgDataOffs+pxnum] & 0xFF;
-				image.setRGB(pxnum%width, pxnum/width, colorMap[colorIndex]);
-			} catch(Exception e) {
-				//break;
+			int colorIndex = decBuf[imgDataOffs+pxnum] & 0xFF;
+			if(colorIndex < colorMapLength) {
+				image.setRGB(pxnum%width, pxnum/width, newColorMap[colorIndex]);
+			}
+			else {
+				// error
+				int a = paletteOobErrorIndex.indexOf(colorIndex);
+				if(a == -1) {
+					paletteOobErrorIndex.add(colorIndex);
+					paletteOobErrorCount.add(1);
+				}
+				else {
+					int b = (int)paletteOobErrorCount.get(a) + 1;
+					paletteOobErrorCount.set(a, b);
+				}
+				//logger.log("Error");
+			}
+		}
+		
+		if(!(paletteOobErrorIndex.isEmpty()))
+		{
+			logger.log("Error: one or more pixels caused an array-index-out-of-bounds error. The color palette is "+colorMapLength+" entries in length. [tgaTranslateColorMappedImg()]");
+			for(int a = 0; a < paletteOobErrorIndex.size(); a++) {
+				logger.log(""+paletteOobErrorCount.get(a)+" pixels use OOB color index "+paletteOobErrorIndex.get(a));
 			}
 		}
 		return image;
